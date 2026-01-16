9c1f09c9724fa35bf348e746d459b9ba5a7ec4c1
youri47 <youri93400@gmail.com>
2026-01-16 00:51:19 +0000

    debugging p.y

---
+++ debugging/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
